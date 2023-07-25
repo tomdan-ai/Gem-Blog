c1308e104c37baac7b21cbc80480f26b9f81b3b2
Tom Daniel <tomudoh258@gmail.com>
2023-07-25 12:00:39 +0100

    Write request-specs for index and show actions in post_conroller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+describe Post, type: :request do
+  describe 'routes with post_controller' do
+    it 'should render the index action correctly' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('posts/index')
+      expect(response.body).to include('All posts by a given user page')
+    end
+
+    it 'should render the show action correctly' do
+      get '/user/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('posts/show')
+      expect(response.body).to include('Single post page')
+    end
   end
 end
