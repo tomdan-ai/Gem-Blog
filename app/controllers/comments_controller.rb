b0fbf30bb7513c8e934874e82f6cf1533293dc94
Tom Daniel <tomudoh258@gmail.com>
2023-07-28 07:41:01 +0100

    Add methods to sucessfully create comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+# app/controllers/comments_controller.rb
+
+class CommentsController < ApplicationController
+  before_action :set_post, only: [:new, :create]
+
+  def new
+    @user = current_user
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author, id: @post), notice: 'Comment was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  # Other methods can go here
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
