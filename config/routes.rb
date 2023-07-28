ca35f8a357ab324d193358841dd624cecdafd327
Tom Daniel <tomudoh258@gmail.com>
2023-07-28 07:39:16 +0100

    Add routes for new features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
+# Rails.application.routes.draw do
+#   get '/user', to: 'user#index', as: 'users'
+#   get '/user/:id', to: 'user#show', as: 'user'
+#   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+#   get '/user/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+# end
+ 
+
 Rails.application.routes.draw do
   get '/user', to: 'user#index', as: 'users'
   get '/user/:id', to: 'user#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/user/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:new, :create]
+    resource :likes, only: [:create]
+  end
 end
- 
