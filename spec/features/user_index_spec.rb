a9645bc6b34cfa5ab599d8469bf5315bcc00befc
Tom Daniel <tomudoh258@gmail.com>
2023-08-01 18:41:29 +0100

    Add Integretion Tests

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,14 +1,42 @@
+require 'rails_helper'
+
 RSpec.describe 'User index page', type: :feature do
-    let!(:user1) { create(:user) }
-    let!(:user2) { create(:user) }
-    let!(:user3) { create(:user) }
-  
-    scenario 'I can see the username of all other users' do
-      visit users_path
-      expect(page).to have_content(user1.username)
-      expect(page).to have_content(user2.username)
-      expect(page).to have_content(user3.username)
-    end
-  
-    # Other scenarios for user index page
+  it 'displays the username of all other users' do
+    user1 = User.create(name: 'User 1')
+    user2 = User.create(name: 'User 2')
+
+    visit users_path
+    expect(page).to have_content(user1.name)
+    expect(page).to have_content(user2.name)
   end
+
+  it 'displays the profile picture for each user' do
+    user1 = User.create(name: 'User 1', photo: 'user1.jpg')
+    user2 = User.create(name: 'User 2', photo: 'user2.jpg')
+
+    visit users_path
+
+    expect(page).to have_selector("img[src='#{user1.photo}']")
+    expect(page).to have_selector("img[src='#{user2.photo}']")
+  end
+
+  it 'displays the number of posts each user has written' do
+    user1 = User.create(name: 'User 1')
+    user1.posts.create(title: 'Post 1', text: 'Text for Post 1')
+    user1.posts.create(title: 'Post 2', text: 'Text for Post 2')
+
+    user2 = User.create(name: 'User 2')
+    user2.posts.create(title: 'Post 3', text: 'Text for Post 3')
+
+    visit users_path
+    expect(page).to have_content("Number Of Posts: 2", count: 1)
+    expect(page).to have_content("Number Of Posts: 1", count: 1)
+  end
+
+  it 'redirects to user show page when clicking on a user' do
+    user = User.create(name: 'User 1')
+    visit users_path
+    click_link 'View Profile'
+    expect(current_path).to eq(user_path(user))
+  end
+end
--- spec/features/user_post_index_spec.rb
+++ spec/features/user_post_index_spec.rb
@@ -1,15 +1,35 @@
-RSpec.describe 'User post index page', type: :feature do
-    let!(:user) { create(:user) }
-    let!(:post1) { create(:post, user: user) }
-    let!(:post2) { create(:post, user: user) }
-    let!(:post3) { create(:post, user: user) }
-  
-    scenario 'I can see a post\'s title' do
-      visit user_posts_path(user)
-      expect(page).to have_content(post1.title)
-      expect(page).to have_content(post2.title)
-      expect(page).to have_content(post3.title)
-    end
-  
-    # Other scenarios for user post index page
+require 'rails_helper'
+
+RSpec.describe "User post index page", type: :feature do
+  let!(:user) { User.create(name: "John Doe") }
+  let!(:post1) { user.posts.create(title: "First Post", text: "Text for First Post") }
+  let!(:post2) { user.posts.create(title: "Second Post", text: "Text for Second Post") }
+ 
+
+  before do
+    visit user_posts_path(user)
   end
+
+  it "displays the user's username" do
+    expect(page).to have_content(user.name)
+  end
+
+
+  it "displays a post's title and some of the post's body" do
+    expect(page).to have_content(post1.title)
+    expect(page).to have_content(post1.text.truncate(100)) 
+  end
+
+  it "displays the first comments on a post" do
+    comment1 = post1.comments.create(author: user, text: "First comment")
+    comment2 = post1.comments.create(author: user, text: "Second comment")
+
+    visit user_posts_path(user)
+
+    expect(page).to have_content(comment1.text)
+    expect(page).to have_content(comment2.text)
+  end
+
+  it "displays a section for pagination if there are more posts than fit on the view" do
+  end
+end
