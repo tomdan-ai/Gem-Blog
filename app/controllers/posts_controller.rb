6984dd86839268a67b9d28f56806fbceebf83c95
Tom Daniel <tomudoh258@gmail.com>
2023-08-01 18:36:28 +0100

    Fix up all n+1 query errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @posts = @user.posts
-    @posts = Post.includes(comments: [:author]).where(author_id: params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @post = @user.posts.includes(:comments).find(params[:id])
   end
+
 
   def new
     @post = Post.new
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,10 @@
-# class UserController < ApplicationController
-#   def index; end
-
-#   def show; end
-# end
-
 class UserController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:posts)
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 end
