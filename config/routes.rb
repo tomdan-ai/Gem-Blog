583f60b2e07a72a516ead935cad80bbfacdd3bbb
Tom Daniel <tomudoh258@gmail.com>
2023-08-04 11:18:34 +0100

    Rewrite routes with destroy resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   root 'user#index'
 
   get '/user', to: 'user#index', as: 'users'
-  get '/user/:id', to: 'user#show', as: 'user'
+  get '/users/:id', to: 'user#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/user/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
-  resources :posts, only: [:new, :create] do
-    resources :comments, only: [:new, :create]
+  resources :posts, only: [:new, :create, :destroy] do
+    resources :comments, only: [:new, :create, :destroy]
     resource :likes, only: [:create]
   end
 end
