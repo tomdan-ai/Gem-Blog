a9edd580001fe8de2682fad449d573e6ce2cb092
Tom Daniel <tomudoh258@gmail.com>
2023-08-04 21:45:48 +0100

    Implement requested chnages

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root 'user#index'
-
-#   get '/user', to: 'user#index', as: 'users'
-#   get '/users/:id', to: 'user#show', as: 'user'
-#   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-#   get '/user/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
-#   resources :posts, only: [:new, :create, :destroy] do
-#     resources :comments, only: [:new, :create, :destroy]
-#     resource :likes, only: [:create]
-#   end
-# end
-
-
-
-
 Rails.application.routes.draw do
   devise_for :users
   root 'user#index'
@@ -30,12 +12,16 @@ Rails.application.routes.draw do
     resource :likes, only: [:create]
   end
 
-  namespace :api, defaults: { format: 'json' } do
-    resources :users, only: [] do
-      resources :posts, only: [:index]
-    end
-    resources :posts, only: [] do
-      resources :comments, only: [:index, :create]
+  namespace :api do
+    namespace :v1 do
+      get 'comments/index'
+      get 'comments/create'
+      get 'posts/index'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: %i[index create]
+        end
+      end
     end
   end
 end
